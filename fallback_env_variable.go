d6dc4bb3f573ac8a150d8f5680a9ee6e72703506
Sebastian Erhardt <sebastian.erhardt@stanford.edu>
2020-10-01 14:55:53 +0200

    + set fallback variable

--- fallback_env_variable.go
+++ fallback_env_variable.go
@@ -9,6 +9,10 @@ func FallbackEnvVariable(envKey, fallbackValue string) string {
 	result := os.Getenv(envKey)
 	if len(result) == 0 {
 		log.Println("Using fallback value", fallbackValue, "for env variable", envKey)
+		err := os.Setenv(envKey, fallbackValue)
+		if err != nil {
+			log.Fatal("can not set fallback variable", envKey, fallbackValue)
+		}
 		return fallbackValue
 	}
 	return result
